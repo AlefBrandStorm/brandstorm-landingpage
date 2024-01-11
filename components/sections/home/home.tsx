41244076e79c8625de234ab1cac476337ae392a3
Alef185 <alefsouzamac1023@gmail.com>
2024-01-10 23:35:40 -0300

    feat add animated svg in home

--- components/sections/home/home.tsx
+++ components/sections/home/home.tsx
@@ -6,16 +6,16 @@ const HomeSection = () => {
         <div className={styles.container}>
             <div className={styles.content}>
                 <div className={styles.textContainer}>
-                    <h1 className={styles.title}>“Transforme sua visão em realidade
-                        com um design excepcional.”</h1>
-                    <p>Bem-vindo à BrandStorm, onde ideias ganham vida através do poder do design. Seja a mudança que seus clientes desejam ver no mundo.</p>
+                    <h1 className={styles.title}>“<span style={{ color: '#B6FF58' }}>Transforme</span> sua <span style={{ backgroundColor: '#F35A79', color: '#FFFFFF' }}>visão</span> em realidade
+                        com um <span style={{ color: '#52B6FF' }}>design</span> <span style={{ color: '#FFB444' }}>excepcional</span>.”</h1>
+                    <p>Bem-vindo à <strong>BrandStorm</strong>, onde ideias ganham vida através do poder do design. Seja a mudança que seus clientes desejam ver no mundo.</p>
                     <a className={styles.button}>Saiba mais</a>
                 </div>
                 <div className={styles.imageContainer}>
                     <Image
-                        src="/logo.svg"
-                        width={500}
-                        height={270}
+                        src="/background-lines.svg"
+                        width={800}
+                        height={800}
                         alt="logo"
                         className={styles.logo}
                     />
